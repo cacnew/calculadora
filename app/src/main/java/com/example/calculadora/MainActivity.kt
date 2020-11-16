99ce6d9abcba184aefba0cdcc387914941017b51
Carlos costa <cacnew@gmail.com>
2020-11-16 18:22:36 -0300

    Calculando operações básicas

--- app/src/main/java/com/example/calculadora/MainActivity.kt
+++ app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -2,35 +2,86 @@ package com.example.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import android.widget.TextView
 import net.objecthunter.exp4j.ExpressionBuilder
 
 class MainActivity : AppCompatActivity() {
+    // Mostra o resultado apertando " = "
 
+    private fun makeCalc(str: String) :String{
+        var result: String
+        var str  = str.replace(",",".").replace("X","*")
+        val hasError:Boolean
 
+        try {
+            val calc = ExpressionBuilder(str).build()
+            result = calc.evaluate().toString()
+
+        } catch (e: Exception) {
+            hasError = true
+            result = "Operação inválida:"+ e.message.toString()
+        }
+
+        return result.replace(".0","").replace(".",",")
+    }
+
+    @ExperimentalStdlibApi
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val tvBotao0 = findViewById(R.id.tv15) as TextView
-        val tvBotao1 = findViewById(R.id.tv9) as TextView
-        val tvBotao2 = findViewById(R.id.tv8) as TextView
-        val tvBotao3 = findViewById(R.id.tv7) as TextView
-        val tvBotao4 = findViewById(R.id.tv6) as TextView
-        val tvBotao5 = findViewById(R.id.tv5) as TextView
-        val tvBotao6 = findViewById(R.id.tv4) as TextView
-        val tvBotao7 = findViewById(R.id.tv) as TextView
-        val tvBotao8 = findViewById(R.id.tv2) as TextView
-        val tvBotao9 = findViewById(R.id.tv3) as TextView
-        val tvVisor = findViewById(R.id.tv36) as TextView
-        val tvClear = findViewById(R.id.tv20) as TextView
+        val tvBotao0    = findViewById(R.id.tv15) as TextView
+        val tvBotao1    = findViewById(R.id.tv9) as TextView
+        val tvBotao2    = findViewById(R.id.tv8) as TextView
+        val tvBotao3    = findViewById(R.id.tv7) as TextView
+        val tvBotao4    = findViewById(R.id.tv6) as TextView
+        val tvBotao5    = findViewById(R.id.tv5) as TextView
+        val tvBotao6    = findViewById(R.id.tv4) as TextView
+        val tvBotao7    = findViewById(R.id.tv) as TextView
+        val tvBotao8    = findViewById(R.id.tv2) as TextView
+        val tvBotao9    = findViewById(R.id.tv3) as TextView
+        val tvVirgula   = findViewById(R.id.tv14) as TextView
+        val tvVisor     = findViewById(R.id.tv36) as TextView
+        val tvHistory   = findViewById(R.id.tvHistory) as TextView
+        val tvClear     = findViewById(R.id.tv20) as TextView
+        tvHistory.setMovementMethod( ScrollingMovementMethod());
+
+
 
         //Operadores
         val tvAdicao         = findViewById(R.id.tv12) as TextView
         val tvMultiplicacao = findViewById(R.id.tv10) as TextView
         val tvSubtracao     = findViewById(R.id.tv11) as TextView
         val tvDivisao       = findViewById(R.id.tv17) as TextView
+
+        val tvEqual = findViewById(R.id.tv13) as TextView
+        val fifo = ArrayDeque<String>()
+
+
+        tvEqual.setOnClickListener {
+
+
+            val visorAtual = tvVisor.text
+            var history = tvHistory.text
+            val resultado = makeCalc(visorAtual.toString())
+            tvVisor.setText(resultado)
+
+            //history = history.toString() + " \n " + visorAtual.toString()
+            fifo.add(visorAtual.toString())
+
+            if (fifo.size == 10){
+                fifo.removeFirst()
+            }
+            var texto = ""
+            fifo.map{
+                texto = it+"\n"+texto
+            }
+            tvHistory.setText(texto)
+
+        }
+
         tvAdicao.setOnClickListener {
 
             val valorAtual = tvVisor.text
@@ -133,6 +184,14 @@ class MainActivity : AppCompatActivity() {
             val resultado = valorAtual.toString()+""+valorBotao.toString()
             tvVisor.setText(resultado)
         }
+        tvVirgula.setOnClickListener {
+            //Toast.makeText(this@MainActivity, "You clicked on TextView 'Click Me'.", Toast.LENGTH_SHORT).show()
+            val valorAtual = tvVisor.text
+            val valorBotao = tvVirgula.text
+            val resultado = valorAtual.toString()+""+valorBotao.toString()
+            tvVisor.setText(resultado)
+        }
+
 
 
     }
